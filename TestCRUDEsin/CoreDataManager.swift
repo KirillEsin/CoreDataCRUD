0b9550663c83d0c643a84f618254145a577ce196
KirillEsin <brahopru@gmail.com>
2017-09-07 01:04:09 +0300

    add car functions

--- TestCRUDEsin/CoreDataManager.swift
+++ TestCRUDEsin/CoreDataManager.swift
@@ -11,6 +11,7 @@ import CoreData
 
 class CoreDataManager {
     
+    //Constants
     private var context: NSManagedObjectContext?
     private let ownerEntityName = "Owner"
     private let carEntityName = "Car"
@@ -22,60 +23,109 @@ class CoreDataManager {
     // MARK: Owner
     
     func getAllOwners() -> [Owner]? {
-        var ordersList: [Owner]?
-        guard let context = context else { return ordersList }
+        var ownersList: [Owner]?
+        guard let context = context else { return ownersList }
         
+        //get by entity
         let request = NSFetchRequest<Owner>.init(entityName: ownerEntityName)
         do {
-            ordersList = try context.fetch(request)
+            ownersList = try context.fetch(request)
         } catch let error { print (error) }
         
-        return ordersList
+        return ownersList
     }
     
-    func creatOwner(name: String) -> Bool {
-        guard let context = context else { return false}
+    func creatOwner(name: String) -> Owner? {
+        guard let context = context else { return nil}
         
-        var result = false
-        
+        //create owner
         let newOwner = NSEntityDescription.insertNewObject(forEntityName: ownerEntityName, into: context) as! Owner
         newOwner.name = name
         
         do {
             try context.save()
-            result = true
         } catch let error { print(error) }
         
-        return result
+        return newOwner
     }
     
-    func updateOwner(owner: Owner, newName: String) -> Bool {
-        guard let context = context else { return false}
-        
-        var result = false
+    func updateOwner(owner: Owner, newName: String) -> Owner? {
+        guard let context = context else { return nil}
         
         owner.name = newName
         
         do {
             try context.save()
-            result = true
         } catch let error { print(error) }
         
-        return result
+        return owner
     }
     
-    func removeOwner(owner: Owner) -> Bool {
-        guard let context = context else { return false }
-        var result = false
+    func removeOwner(owner: Owner) {
+        guard let context = context else { return }
         
         context.delete(owner)
         
         do {
             try context.save()
-            result = true
         } catch let error { print(error) }
-        
-        return result
     }
     
+    // MARK: Car
+    
+    func creatCar(name: String) -> Car? {
+        guard let context = context else { return nil }
+        
+        let newCar = NSEntityDescription.insertNewObject(forEntityName: carEntityName, into: context) as! Car
+        newCar.name = name
+        
+        do {
+            try context.save()
+        } catch let error { print(error) }
+        
+        return newCar
+    }
+    
+    func getAllCars() -> [Car]? {
+        var carsList: [Car]?
+        guard let context = context else { return carsList }
+        
+        let request = NSFetchRequest<Car>.init(entityName: carEntityName)
+        do {
+            carsList = try context.fetch(request)
+        } catch let error { print (error) }
+        
+        return carsList
+    }
+    
+    func removeCar(car: Car) {
+        guard let context = context else { return }
+        
+       let fetchRequest = NSFetchRequest<Owner>.init(entityName: ownerEntityName)
+        
+        //Use predicate
+        //let predicate = NSPredicate.init(format: "SELF.name == %@", car.name!)
+        //fetchRequest.predicate = predicate
+        
+        do {
+            let elements = try context.fetch(fetchRequest)
+            for obj in elements {
+                context.delete(car)
+            }
+            try context.save()
+        } catch let error { print(error) }
+    }
+    
+    func updateCar(car: Car, newName: String) -> Car? {
+        guard let context = context else { return nil }
+
+        
+        car.name = newName
+        
+        do {
+            try context.save()
+        } catch let error { print(error) }
+        
+        return car
+    }
 }
